2fc60ad0537106f2ec9bb2eccfbe8781e42fbf21
Alisher-00kg <alisher.estebesov@gmail.com>
2025-05-23 23:15:30 +0300

    create and fixed styles innerpage jsx, card jsx, create scrollToTop component

--- src/components/UI/button/Button.jsx
+++ src/components/UI/button/Button.jsx
@@ -18,11 +18,30 @@ export default Button;
 const StyledButton = styled(MuiButton)(({ variant }) => ({
   "&.MuiButtonBase-root": {
     width: "100%",
-    height: variant === "base" ? "31px" : "fit-content",
-    fontSize: "16px",
+    height:
+      variant === "base"
+        ? "31px"
+        : variant === "showmore"
+        ? "46px"
+        : "fit-content",
+    fontSize: variant === "showmore" ? "18px" : "16px",
     fontWeight: "500",
     borderRadius: "10px",
-    color: variant === "base" ? "#fff" : "",
-    background: variant === "base" ? "#03045E" : "",
+    color:
+      variant === "base"
+        ? "#fff"
+        : variant === "text"
+        ? "#7E52FF"
+        : variant === "showmore"
+        ? "#03045E"
+        : "",
+    background:
+      variant === "base" ? "#03045E" : variant === "showmore" ? "#FFF902" : "",
+    "& span": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: "6px",
+    },
   },
 }));
--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { AdminHeader } from "../components/header/admin/AdminHeader";
 import styled from "styled-components";
+import ScrollToTop from "../components/UI/scroll-to-top/ScrollToTop";
 
 export const AdminLayout = () => {
   return (
-    <div>
-      <AdminHeader />
-      <StyledOutletWrapper>
-        <Outlet />
-      </StyledOutletWrapper>
-    </div>
+    <>
+      <ScrollToTop />
+      <div>
+        <AdminHeader />
+        <StyledOutletWrapper>
+          <Outlet />
+        </StyledOutletWrapper>
+      </div>
+    </>
   );
 };
 const StyledOutletWrapper = styled.div`
